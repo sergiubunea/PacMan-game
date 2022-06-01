367af90e80a7f7cf4039b54e62e3751936490e69
sergiubunea <buneasergiu1@gmail.com>
2022-06-01 16:20:58 +0300

    Ghost part 2

--- src/Enemy.js
+++ src/Enemy.js
@@ -1,3 +1,5 @@
+import Direction from "./Direction.js";
+
 export default class Enemy {
     constructor(x, y, tileSize, viteza, tileMap) {
         this.x = x;
@@ -6,11 +8,39 @@ export default class Enemy {
         this.viteza = viteza;
         this.tileMap = tileMap;
 
+        this.direction = Math.floor(Math.random() * Object.keys(Direction).length);
+        this.directionTimeDefault = this.#random(5, 60)
+
+
         this.#loadImages();
     }
 
     draw(ctx) {
+        this.#move();
         ctx.drawImage(this.image, this.x, this.y, this.tileSize, this.tileSize);
+    }
+
+    #move() {
+        if(!this.tileMap.didCollideWithEnviroment(this.x, this.y, this.Direction)) {
+            switch(this.Direction) {
+                case Direction.up:
+                    this.y -= this.viteza;
+                    break;
+                case Direction.down:
+                    this.y += this.viteza;
+                    break;
+                case Direction.left:
+                    this.x -= this.viteza;
+                    break;
+                case Direction.right:
+                    this.x += this.viteza;
+                    break;
+            }
+        }
+    }
+
+    #random(min, max) {
+        return Math.floor(Math.random() * (max + 1)) + min;
     }
 
     #loadImages() {
